dfebf487069137fef002f9f063da89f2f2627e8c
aluba0609 <164868966@qq.com>
2021-06-05 11:29:12 +0800

    [skip ci]

--- main.js
+++
@@ -1,3 +0,0 @@
-
-const show = require('./src/show.js');
-show('Webpack');
---
+++ src/index.js
@@ -0,0 +1,13 @@
+require('./index.css')
+
+let a=require('./a.js')
+console.log(a)
+console.log(1)
+
+class AB {
+    constructor(){
+        this.a=1
+    }
+}
+let ab=new AB();
+console.log(ab)
